1a2828fbf21cc506e80a8eb12e0c918f4c73856b
Vikas Katariya <vikas.katariya@arm.com>
2020-01-16 10:13:40 +0000

    Patch to get IPC Semaphore initialized.
    This is only related to TFM_TWINCPU NS mbed-os image.
    This patch helps to initialize the semaphore required for the Mailbox,
    was resulting in the Hard fault on the platform.
    
    Signed-off-by: Vikas Katariya <vikas.katariya@arm.com>

--- targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CPROTO_064_SB/device/COMPONENT_CM4/system_psoc6_cm4.c
+++ targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CPROTO_064_SB/device/COMPONENT_CM4/system_psoc6_cm4.c
@@ -194,19 +194,6 @@ void SystemInit(void)
 
 #if !defined(CY_IPC_DEFAULT_CFG_DISABLE)
 
-#ifdef __CM0P_PRESENT
-    #if (__CM0P_PRESENT == 0)
-        /* Allocate and initialize semaphores for the system operations. */
-        static uint32_t ipcSemaArray[CY_IPC_SEMA_COUNT / CY_IPC_SEMA_PER_WORD];
-        (void) Cy_IPC_Sema_Init(CY_IPC_CHAN_SEMA, CY_IPC_SEMA_COUNT, ipcSemaArray);
-    #else
-        (void) Cy_IPC_Sema_Init(CY_IPC_CHAN_SEMA, 0ul, NULL);
-    #endif /* (__CM0P_PRESENT) */
-#else
-    (void) Cy_IPC_Sema_Init(CY_IPC_CHAN_SEMA, 0ul, NULL);
-#endif /* __CM0P_PRESENT */
-
-
     /********************************************************************************
     *
     * Initializes the system pipes. The system pipes are used by BLE and Flash.

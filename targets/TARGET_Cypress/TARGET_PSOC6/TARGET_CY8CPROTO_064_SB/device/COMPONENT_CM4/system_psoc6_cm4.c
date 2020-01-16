d1ff232a5d389badcd55df409fc45901b0aeeb91
Vikas Katariya <vikas.katariya@arm.com>
2020-01-16 10:13:40 +0000

    Patch to get IPC Semaphore initialized.
    This is only related to TFM_TWINCPU NS mbed-os image and applies to
    only this target - CY8CPROTO_064_SB
    This patch helps to initialize the semaphore required for the IPC,
    Mailbox and Interface initialization, which was resulting in the
    Hard fault on the platform during the bring up.
    
    Signed-off-by: Vikas Katariya <vikas.katariya@arm.com>

--- targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CPROTO_064_SB/device/COMPONENT_CM4/system_psoc6_cm4.c
+++ targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CPROTO_064_SB/device/COMPONENT_CM4/system_psoc6_cm4.c
@@ -171,7 +171,7 @@ void SystemInit(void)
 #if !defined(CY_IPC_DEFAULT_CFG_DISABLE)
 
 #ifdef __CM0P_PRESENT
-    #if (__CM0P_PRESENT == 0)
+    #if (__CM0P_PRESENT == 1)
         /* Allocate and initialize semaphores for the system operations. */
         static uint32_t ipcSemaArray[CY_IPC_SEMA_COUNT / CY_IPC_SEMA_PER_WORD];
         (void) Cy_IPC_Sema_Init(CY_IPC_CHAN_SEMA, CY_IPC_SEMA_COUNT, ipcSemaArray);

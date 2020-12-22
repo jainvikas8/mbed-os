7d4d9bdfbc2e7ec26699a1daab72601b9c224094
Vikas Katariya <Vikas.katariya@arm.com>
2020-12-22 15:11:36 +0000

    ARM_MUSCA_B1 : Add new files to TFM_V1_2
    These files are generated by the build_tfm.py when using
    ConfigCoreIPC.cmake - TF-M

---
+++ targets/TARGET_ARM_SSG/TARGET_MUSCA_B1/partition/signing_layout_s.c
@@ -0,0 +1,25 @@
+# 1 "/data_sda/Workspace/mbed-os-tf-m-regression-tests/mbed-os/platform/FEATURE_EXPERIMENTAL_API/FEATURE_PSA/TARGET_TFM/TARGET_TFM_V1_2/TARGET_IGNORE/trusted-firmware-m/cmake_build/bl2/ext/mcuboot/signing_layout_s.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/data_sda/Workspace/mbed-os-tf-m-regression-tests/mbed-os/platform/FEATURE_EXPERIMENTAL_API/FEATURE_PSA/TARGET_TFM/TARGET_TFM_V1_2/TARGET_IGNORE/trusted-firmware-m/cmake_build/bl2/ext/mcuboot/signing_layout_s.c"
+
+
+
+
+
+
+# 1 "/data_sda/Workspace/mbed-os-tf-m-regression-tests/mbed-os/platform/FEATURE_EXPERIMENTAL_API/FEATURE_PSA/TARGET_TFM/TARGET_TFM_V1_2/TARGET_IGNORE/trusted-firmware-m/platform/ext/target/musca_b1/partition/flash_layout.h" 1
+# 8 "/data_sda/Workspace/mbed-os-tf-m-regression-tests/mbed-os/platform/FEATURE_EXPERIMENTAL_API/FEATURE_PSA/TARGET_TFM/TARGET_TFM_V1_2/TARGET_IGNORE/trusted-firmware-m/cmake_build/bl2/ext/mcuboot/signing_layout_s.c" 2
+
+
+
+enum image_attributes {
+    RE_SECURE_IMAGE_OFFSET = (0x0),
+    RE_SECURE_IMAGE_MAX_SIZE = (0x60000),
+    RE_NON_SECURE_IMAGE_OFFSET = ((0x0) + (0x60000)),
+    RE_NON_SECURE_IMAGE_MAX_SIZE = (0x80000),
+
+
+
+    RE_SIGN_BIN_SIZE = ((0x60000)),
+};
--- tools/psa/tfm/bin_utils/imgtool/__init__.py
+++ tools/psa/tfm/bin_utils/imgtool/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Linaro Limited
+# Copyright 2017-2020 Linaro Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This file is intentionally empty.
-#
-# The __init__.py files are required to make Python treat the directories as
-# containing packages.
+imgtool_version = "1.7.0rc1"
